5f30ddd6825c8110019fec11d34c95ab7c549de2
takuya0216 <takuya0034@gmail.com>
2024-05-06 16:45:01 +0900

    done

---
+++ app/hooks/useTodos.ts
@@ -0,0 +1,21 @@
+import { API_URL } from "@/constants/urls";
+import useSWR from "swr";
+
+async function fetcher(key: string) {
+    return fetch(key).then((res) => res.json());
+}
+
+export const useTodos = () => { 
+    const {data, isLoading, error, mutate} = useSWR(
+        `${API_URL}/allTodos`,
+         fetcher
+    );
+
+    return {
+        todos: data,
+        isLoading,
+        error,
+        mutate
+    }
+
+}
